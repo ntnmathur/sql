9b978ab11dcf93e9bf44c7cbacde10130f7cc23c
Mathur <ntnmathur@gmail.com>
2017-09-11 22:45:06 -0700

    syncing

--- problems.sql
+++ problems.sql
@@ -1,4 +1,4 @@
--- From City table explode the data from start date and end date
+-- Question(1) From City table explode the data from start date and end date
 
 select * from city;
 
@@ -71,4 +71,89 @@ WHERE explode_data IS NOT NULL;
 -- seattle	24
 -- seattle	25
 
+-- Question(2) Department and Employees
+SELECT * FROM dept;
+SELECT count(*) FROM emp;
+
+-- Number of employees in each dept
+SELECT
+  dname,
+  count(*)
+from dept d
+INNER JOIN emp e
+  on d.deptno = e.deptno
+GROUP BY dname;
+
+-- Employee with not a valid dept
+SELECT *
+FROM emp e
+LEFT JOIN dept d
+  ON e.deptno = d.deptno
+WHERE d.deptno IS NULL;
+
+-- Top Salary in each dept
+SELECT
+dname,
+max(e.sal)
+FROM dept d
+INNER JOIN emp e
+  on d.deptno = e.deptno
+GROUP BY dname;
+
+
+-- Which employee has top salary in each dept
+
+-- GROUP BY
+SELECT
+  e.ename,
+  e.sal,
+  X.dname
+FROM emp e
+INNER JOIN
+(SELECT
+d.deptno,
+d.dname,
+max(e.sal) max_sal
+FROM dept d
+INNER JOIN emp e
+on d.deptno = e.deptno
+GROUP BY d.deptno,d.dname) X
+  ON e.deptno = X.deptno
+and e.sal = X.max_sal;
+
+-- RANK
+
+SELECT
+  e.ename,
+  d.dname,
+  e.sal,
+  dense_rank() over(PARTITION BY d.deptno ORDER BY sal DESC) sal_rank
+  FROM emp e
+INNER JOIN dept d
+    on e.deptno = d.deptno;
+
+-- Salary of employee per job or per dept - if max its high, if min its low
+select
+  X.*,
+  case
+  when sal = max_sal_dept THEN 'HIGH IN DEPT'
+  when sal = min_sal_dept THEN 'LOW IN DEPT'
+  ELSE 'AVERAGE IN DEPT' END dept_indicator,
+  case
+  when sal = max_sal_job THEN 'HIGH IN JOB'
+  when sal = min_sal_job THEN 'LOW IN JOB'
+  ELSE 'AVERAGE IN JOB' END job_indicator
+
+  from
+(SELECT
+  deptno,
+  ename,
+  job,
+  sal,
+  max(sal) OVER (PARTITION BY deptno) max_sal_dept,
+  min(sal) OVER (PARTITION BY deptno) min_sal_dept,
+  max(sal) OVER (PARTITION BY job) max_sal_job,
+  min(sal) OVER (PARTITION BY job) min_sal_job
+
+from emp) X;
 
